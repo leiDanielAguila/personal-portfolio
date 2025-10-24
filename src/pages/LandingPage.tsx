e2ce080c062f24c7e4036adffae7e17eebaf4951
Lei Aguila <aguila.christleidaniel@gmail.com>
2025-10-25 01:54:49 +0800

    initial commit

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import { AppShell } from "@mantine/core";
+
+function LandingPage() {
+  return (
+    <AppShell>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  );
+}
+
+export default LandingPage;
