040f16e0031249249dbeeba8f9d9f410116831b5
christlei aguila <aguila.christleidaniel@gmail.com>
2026-04-13 17:58:17 +0800

    Bugfix: - changed the root of the island nav

--- src/pages/InformationClientPage.tsx
+++ src/pages/InformationClientPage.tsx
@@ -6,12 +6,15 @@ function InformationClientPage() {
   return (
     <main className="min-h-screen flex flex-col items-center px-4 py-6 gap-4">
       <div className="w-full max-w-6xl flex flex-col md:flex-row items-stretch gap-4">
-        <div className="w-full md:flex-1 flex flex-col md:flex-row items-start gap-4 ">
+        <div className="w-full md:flex-1 flex flex-col md:flex-row items-start gap-4">
           <IslandNavigation />
         </div>
-      </div>
-      <div className="fixed bottom-4 right-4">
-        <Chatbot />
+        <div className="w-full flex flex-row p-6">
+         
+        </div>
+        <div className="fixed bottom-4 right-4">
+          <Chatbot />
+        </div>
       </div>
     </main>
   );
